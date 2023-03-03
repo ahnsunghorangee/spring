ff80ff71e2a859cff9a7ddc8e353fc38b5688d55
SungHo Ahn <jugiaro95@gmail.com>
2023-03-03 23:54:39 +0900

    [spring01-core01 :o: AppConfig Refactoring

--- spring01-core01/core/src/main/java/com/inflearn/core/AppConfig.java
+++ spring01-core01/core/src/main/java/com/inflearn/core/AppConfig.java
@@ -1,5 +1,6 @@
 package com.inflearn.core;
 
+import com.inflearn.core.discount.DiscountPolicy;
 import com.inflearn.core.discount.FixDiscountPolicy;
 import com.inflearn.core.member.MemberService;
 import com.inflearn.core.member.MemberServiceImpl;
@@ -8,14 +9,30 @@ import com.inflearn.core.order.OrderService;
 import com.inflearn.core.order.OrderServiceImpl;
 
 public class AppConfig { // App 전체를 설정 및 구성
-
+    /*
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해 객체가 인스턴스를 주입? (= 생성자 주입)
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
+    */
 
+    // 위 코드 리팩토링 (중복 제거 및 역할에 따른 구현이 보이도록)
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
 
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
