22bab89ccab3b0889d83fc68f98426f9935bc0b4
SungHo Ahn <jugiaro95@gmail.com>
2023-03-04 11:18:52 +0900

    [spring01-core01] :o: Find Duplicate Bean Type

--- spring01-core01/core/src/test/java/com/inflearn/core/beanfind/ApplicationContextBasicTest.java
+++ spring01-core01/core/src/test/java/com/inflearn/core/beanfind/ApplicationContextBasicTest.java
@@ -3,14 +3,13 @@ package com.inflearn.core.beanfind;
 import com.inflearn.core.AppConfig;
 import com.inflearn.core.member.MemberService;
 import com.inflearn.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * 스프링 컨테이너에서 스프링 빈 찾기
@@ -21,7 +20,7 @@ public class ApplicationContextBasicTest {
 
     @Test
     @DisplayName("빈 이름 조회")
-    void findBeanByName(){
+    void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         System.out.println("memberService = " + memberService);
         System.out.println("memberService.getClass() = " + memberService.getClass());
@@ -30,21 +29,21 @@ public class ApplicationContextBasicTest {
 
     @Test
     @DisplayName("이름 없이 타입으로만 조회")
-    void findBeanByType(){
+    void findBeanByType() {
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
     @DisplayName("인터페이스 구체 타입으로 조회")
-    void findBeanByName2(){
+    void findBeanByName2() {
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
     @DisplayName("빈 이름으로 조회 X")
-    void findBeanByNameX(){
+    void findBeanByNameX() {
         // 두번째 파라미터가 실행됬을 때, 첫번째 파라미터 에러가 나면 성공한 것
         assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberService.class));
     }
