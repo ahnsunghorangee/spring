7954320e4d3a84eb6c02162e5807b93c69d20f9e
SungHo Ahn <jugiaro95@gmail.com>
2023-12-29 11:20:04 +0900

    [springcloud01-msa] 10-01. Rest Template

--- springcloud01-msa/user-service/src/main/java/com/inflearn/userservice/service/UserServiceImpl.java
+++ springcloud01-msa/user-service/src/main/java/com/inflearn/userservice/service/UserServiceImpl.java
@@ -7,11 +7,16 @@ import com.inflearn.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +27,15 @@ public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
+    Environment env;
+    RestTemplate restTemplate;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment env, RestTemplate restTemplate){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.env = env;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -54,7 +63,20 @@ public class UserServiceImpl implements UserService {
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> ordersList = new ArrayList<>();
+        /* Using as RestTemplate */
+        String orderUrl = String.format(env.getProperty("order_service.url"), userId); // String.format: order_service.url에 들어있는 변수(%s)에 값을 넣기 위함
+        /**
+         * exchange() Desc
+         * 매개변수1: 주소값
+         * 매개변수2: HTTP Method
+         * 매개변수3: 요청할 때 파라미터
+         * 매개변수4: 전달받을 형식
+         */
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ResponseOrder>>() {
+        });
+        List<ResponseOrder> ordersList = orderListResponse.getBody();
         userDto.setOrders(ordersList);
 
         return userDto;
