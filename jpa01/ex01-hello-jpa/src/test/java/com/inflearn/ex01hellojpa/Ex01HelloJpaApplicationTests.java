e77681f23e686001ee8961020da3a2851064ee23
SungHo Ahn <jugiaro95@gmail.com>
2024-01-08 23:45:10 +0900

    [jpa01] 02-01. Hello JPA 프로젝트 생성

---
+++ jpa01/ex01-hello-jpa/src/test/java/com/inflearn/ex01hellojpa/Ex01HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.ex01hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
